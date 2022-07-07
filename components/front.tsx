e79e8f42d8053c63f9955bba7c1daeac5bb39269
Thamognya Kodi <contact@thamognya.com>
2022-07-07 19:16:25 +0700

    update

--- components/front.tsx
+++ components/front.tsx
@@ -22,23 +22,23 @@ const Front = () => {
             <a href="https://links.thamognya.com" target="_blank" rel="noreferrer" >
                 <img className="rounded-full hover:scale-125 transform-gpu" src="/pfp.jpeg" width="100" />
             </a>
-            <a href="https://www.thamognya.com" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 rounded-full bg-zinc-400 bg-opacity-40 flex items-center">
+            <a href="https://www.thamognya.com" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 hover:scale-125 rounded-full bg-zinc-400 bg-opacity-40 flex items-center">
                 <CgWebsite />
                 <h1 className="pl-2">Website</h1>
             </a>
-            <a href="https://github.com/Thamognya" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 rounded-full bg-gray-900 text-slate-50 bg-opacity-75 flex items-center">
+            <a href="https://github.com/Thamognya" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 hover:scale-125 rounded-full bg-gray-900 text-slate-50 bg-opacity-75 flex items-center">
                 <FaGithub />
                 <h1 className="pl-2">Github</h1>
             </a>
-            <a href="https://git.thamognya.com" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 rounded-full bg-lime-600 text-slate-50 bg-opacity-75 flex items-center">
+            <a href="https://git.thamognya.com" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 hover:scale-125 rounded-full bg-lime-600 text-slate-50 bg-opacity-75 flex items-center">
                 <SiGitea />
                 <h1 className="pl-2">Gitea</h1>
             </a>
-            <a href="https://github.com/Thamognya" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 rounded-full bg-sky-600 text-slate-50 bg-opacity-75 flex items-center">
+            <a href="https://github.com/Thamognya" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 hover:scale-125 rounded-full bg-sky-600 text-slate-50 bg-opacity-75 flex items-center">
                 <FaLinkedin />
                 <h1 className="pl-2">LinkedIn</h1>
             </a>
-            <a href="https://blog.thamognya.com" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 rounded-full bg-violet-600 text-slate-50 bg-opacity-75 flex items-center">
+            <a href="https://blog.thamognya.com" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 hover:scale-125 rounded-full bg-violet-600 text-slate-50 bg-opacity-75 flex items-center">
                 <FaBlogger />
                 <h1 className="pl-2">Blog</h1>
             </a>
