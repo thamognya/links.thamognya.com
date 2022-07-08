8186b8f4254d2fd566a3473c183c65642bd60972
Thamognya Kodi <contact@thamognya.com>
2022-07-08 08:01:02 +0700

    update

--- components/front.tsx
+++ components/front.tsx
@@ -13,12 +13,16 @@ import {
     CgWebsite
 } from "react-icons/cg";
 
+import {
+    BsFiles,
+} from "react-icons/bs";
+
 // change frmo h1 to a to link everything
 
 const Front = () => {
     return (
     <>
-        <div className="grid md:grid-cols-3 grid-cols-1 gap-y-2 gap-x-10 place-items-center h-screen">
+        <div className="grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-y-2 gap-x-10 place-items-center h-screen">
             <a href="https://links.thamognya.com" target="_blank" rel="noreferrer" >
                 <img className="rounded-full hover:scale-125 transform-gpu" src="/pfp.jpeg" width="100" />
             </a>
@@ -34,13 +38,17 @@ const Front = () => {
                 <SiGitea />
                 <h1 className="pl-2">Gitea</h1>
             </a>
-            <a href="https://github.com/Thamognya" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 hover:scale-125 rounded-full bg-sky-600 text-slate-50 bg-opacity-75 flex items-center">
+            <a href="www.linkedin.com/in/thamognya" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 hover:scale-125 rounded-full bg-sky-600 text-slate-50 bg-opacity-75 flex items-center">
                 <FaLinkedin />
                 <h1 className="pl-2">LinkedIn</h1>
             </a>
             <a href="https://blog.thamognya.com" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 hover:scale-125 rounded-full bg-violet-600 text-slate-50 bg-opacity-75 flex items-center">
                 <FaBlogger />
                 <h1 className="pl-2">Blog</h1>
+            </a>
+            <a href="https://www.thamognya.com/cv.pdf" target="_blank" rel="noreferrer" className="py-5 md:px-10 px-20 hover:scale-125 rounded-full bg-red-600 text-slate-50 bg-opacity-75 flex items-center">
+                <BsFiles />
+                <h1 className="pl-2">CV</h1>
             </a>
         </div>
     </>
