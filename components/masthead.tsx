f11b2d446deff51e551575c43688f55d2700d572
Thamognya <98194153+thamognya@users.noreply.github.com>
2022-07-17 18:30:41 +0700

    Update masthead.tsx

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 import Image from 'next/image'
 import Front from './front'
 
-const masthead: React.FC = () => {
+const LazyVideoBg = dynamic(() => import('./videoBg'), {
+    ssr: true
+})
+
+const masthead = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center">
             <video
@@ -13,7 +17,7 @@ const masthead: React.FC = () => {
                 playsInline
                 className="absolute w-screen h-screen object-cover"
             >
-                <source src="/bg.mp4" type="video/mp4" />
+                <LazyVideoBg />
             </video>
             <div className="font-bold text-black z-10">
                 <Front />
