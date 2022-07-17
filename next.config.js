f9e333c59557cf479b6f45ff287e168d53ed7c0f
Thamognya <98194153+thamognya@users.noreply.github.com>
2022-07-17 18:26:37 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    experimental: {
+        runtime: 'experimental-edge'
+    },
+    images: {
+        domains: ['cdn.jsdelivr.net']
+    }
 }
 
 module.exports = nextConfig
