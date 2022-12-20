57aa06161a10abc08cd9201d28ccf4753a1a81a0
Thamognya Kodi <contact@thamognya.com>
2022-12-20 08:12:13 +0700

    fira code

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
         './components/**/*.{js,ts,jsx,tsx}'
     ],
     theme: {
+        fontFamily: {
+            fira: ['Fira Code']
+        },
         extend: {}
     },
     plugins: []
