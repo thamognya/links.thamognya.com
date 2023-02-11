d797f714f77a6373ec0bb6b024a3b16f25affaa0
Thamognya Kodi <contact@thamognya.com>
2023-02-11 18:12:53 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import { BsLinkedin } from 'react-icons/bs'
+import React from 'react'
 
 const SocialMedia = (props: any) => {
     return (
         <>
-            <div className={`${props.color} hover:scale-125`}>
-                <div className="flex justify-between items-center gap-x-2">
-                    {props.name} {props.children}
-                </div>
-            </div>
-            {props.children}
+            <button
+                className={`${props.color} hover:${props.hoverColor} hover:scale-125 text-white font-bold py-2 px-4 rounded-full transition duration-200 ease-in-out`}
+            >
+                {props.name}
+            </button>
         </>
     )
 }
@@ -31,19 +30,57 @@ export default function Home() {
                     Your browser does not support the video tag.
                 </video>
                 <div className="font-bold text-black z-10 text-3xl">
-                    <div className="grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3   sm:grid-cols-2 grid-cols-2 place-items-center h-screen gap-y-2 gap-x-10">
-                        <SocialMedia name="LinkedIn" color="bg-blue-500">
-                            <BsLinkedin />
-                        </SocialMedia>
-                        <h1>Reddit</h1>
-                        <h1>Github</h1>
-                        <h1>Gitea</h1>
-                        <h1>Gitlab</h1>
-                        <h1>CV</h1>
-                        <h1>Email</h1>
-                        <h1>Blog</h1>
-                        <h1>Research</h1>
-                        <h1>Projects</h1>
+                    <div className="grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 place-items-center h-screen gap-y-2 gap-x-10">
+                        <SocialMedia
+                            name="LinkedIn"
+                            color="bg-blue-500"
+                            hoverColor="bg-blue-700"
+                        />
+                        <SocialMedia
+                            name="Reddit"
+                            color="bg-orange-500"
+                            hoverColor="bg-orange-700"
+                        />
+                        <SocialMedia
+                            name="GitHub"
+                            color="bg-black"
+                            hoverColor="bg-gray-900"
+                        />
+                        <SocialMedia
+                            name="Gitea"
+                            color="bg-green-500"
+                            hoverColor="bg-green-700"
+                        />
+                        <SocialMedia
+                            name="GitLab"
+                            color="bg-red-500"
+                            hoverColor="bg-red-700"
+                        />
+                        <SocialMedia
+                            name="CV"
+                            color="bg-purple-500"
+                            hoverColor="bg-purple-700"
+                        />
+                        <SocialMedia
+                            name="Email"
+                            color="bg-teal-500"
+                            hoverColor="bg-teal-700"
+                        />
+                        <SocialMedia
+                            name="Blog"
+                            color="bg-pink-500"
+                            hoverColor="bg-pink-700"
+                        />
+                        <SocialMedia
+                            name="Research"
+                            color="bg-indigo-500"
+                            hoverColor="bg-indigo-700"
+                        />
+                       <SocialMedia
+                            name="Projects"
+                            color="bg-yellow-500"
+                            hoverColor="bg-yellow-700"
+                        />
                     </div>
                 </div>
             </div>
